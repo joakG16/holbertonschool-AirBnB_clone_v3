c4c60c96eafd455879b378b8da2ddac1251b5b58
JoakG16 <4379@holbertonstudents.com>
2022-12-08 16:39:04 +0000

    task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,26 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+
 @app_views.route('/status')
 def status():
     ''' view function that returns the JSON response status '''
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def stats():
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+    obj_count = {
+        "amenities": storage.count(Amenity), 
+        "cities": storage.count(City), 
+        "places": storage.count(Place), 
+        "reviews": storage.count(Review), 
+        "states": storage.count(State),
+        "users": storage.count(User) 
+    }
+    return jsonify(obj_count)
