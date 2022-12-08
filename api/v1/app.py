0feba91baa08f9a910f643f5403385caf59c28aa
JoakG16 <4379@holbertonstudents.com>
2022-12-08 16:22:43 +0000

    task 4 documentation

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Flask app inizialization
-"""
+""" Flask app inizialization """
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
