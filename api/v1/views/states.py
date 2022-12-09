bf13aa5709c5a17af6acc51f2be8ec0451a8d2c5
joakG16 <4379@holbertonschool.com>
2022-12-08 23:25:49 -0300

    only using filestorage error

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,14 +3,14 @@
 actions '''
 
 from flask import jsonify, abort, request
-from models import FileStorage
+from models import storage
 from models.state import State
 from api.v1.views import app_views
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_all_states():
     ''' retrieve a list of of all State object '''
-    states_dict = [st.to_dict() for st in FileStorage.all(State).values()]
+    states_dict = [st.to_dict() for st in storage.all(State)]
     # retrieve the state objects using all method, and then
     # convert it into a dictionary each for JSON representation
     return jsonify(states_dict)
@@ -18,7 +18,7 @@ def get_all_states():
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state_by_id(state_id):
     ''' retrieve a State obj depending on its id '''
-    obj = FileStorage.get(State, state_id)
+    obj = storage.get(State, state_id)
     if obj:
         return jsonify(obj.to_dict())
     else:
@@ -27,11 +27,11 @@ def get_state_by_id(state_id):
 @app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
 def delete_state_by_index(state_id):
     ''' delete a state obj based on it's id '''
-    obj = FileStorage.get(State, state_id)
+    obj = storage.get(State, state_id)
     if obj is None:
         abort(404)  # returns 404 error
-    FileStorage.delete(obj)
-    FileStorage.save()  # changes need to be saved
+    storage.delete(obj)
+    storage.save()  # changes need to be saved
     return jsonify({}), 200
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
@@ -45,14 +45,14 @@ def create_state():
         abort(400, "Missing Name")
     new_state = State(**state_data)  # creating the new state object
     # with its unpacked data (in key-value pairs) as kwargs
-    FileStorage.new(new_state)
-    FileStorage.save()
+    storage.new(new_state)
+    storage.save()
     return jsonify(new_state.to_dict()), 201
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
     ''' update a state object '''
-    state_obj = FileStorage.get(State, state_id)
+    state_obj = storage.get(State, state_id)
     if state_obj is None:
         abort(404)
     state_data = request.get_json()
@@ -63,5 +63,5 @@ def update_state(state_id):
             pass
         else:
             setattr(state_obj, key, value)
-    FileStorage.save()
+    storage.save()
     return jsonify(state_obj.to_dict()), 200
