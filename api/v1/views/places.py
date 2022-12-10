6a2146599064f8dcc9788df88768180532624c29
joakG16 <4379@holbertonschool.com>
2022-12-10 17:25:30 -0300

    put http method added

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -54,7 +54,7 @@ def delete_place_obj_by_id(place_id):
 @app_views.route('/cities/<city_id>/places', methods=['POST'],
                  strict_slashes=False)
 def post_create_place_object(city_id):
-    ''' Post(create) a new Place object and assign its 
+    ''' Post(create) a new Place object and assign its
     correspondent City id.
     City and User will be checked to ensure if
     the ones given in the request exist in storage
@@ -63,22 +63,44 @@ def post_create_place_object(city_id):
     if city is None:
         abort(404)
 
-    place_data = request.get_json()
-    if place_data is None:
+    place_data_dict = request.get_json()
+    if place_data_dict is None:
         abort(400, "Not a JSON")
-    if 'user_id' not in place_data:
+    if 'user_id' not in place_data_dict:
         abort(400, "Missing user_id")
 
-    user = storage.get(User, place_data['user_id'])
+    user = storage.get(User, place_data_dict['user_id'])
     if user is None:
         abort(404)
 
-    if 'name' not in place_data:
+    if 'name' not in place_data_dict:
         abort(400, "Missing Name")
 
-    place_data['city_id'] = city_id
-    new_place = Place(**place_data)
+    place_data_dict['city_id'] = city_id
+    new_place = Place(**place_data_dict)
 
     storage.new(new_place)
     storage.save()
     return jsonify(new_place.to_dict()), 201
+
+
+@app_views.route('/places/<place_id>', methods=['PUT'],
+                 strict_slashes=False)
+def put_update_place_obj(place_id):
+    ''' update Place instance's atributes given through JSON format
+    (HTTP body request)
+    '''
+    place_to_update = storage.get(Place, place_id)
+    if place_to_update is None:
+        abort(404)
+    updated_place_data_dict = request.get_json()
+    if updated_place_data_dict is None:
+        abort(400, "Not a JSON")
+    for key, value in updated_place_data_dict.items():
+        if key == 'id' or key == 'user_id' or key == 'city_id'\
+             or key == 'created_at' or key == 'updated_at':
+            pass
+        else:
+            setattr(place_to_update, key, value)
+    storage.save()
+    return jsonify(place_to_update.to_dict()), 200
