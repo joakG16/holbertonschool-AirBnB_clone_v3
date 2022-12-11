c8527af4ec9064be03bec84550f00f9600f4270b
joakG16 <4379@holbertonschool.com>
2022-12-11 16:15:34 -0300

    pep8

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 ''' a new view for State objects that handles all default RESTFul API
 actions '''
-
 from flask import jsonify, abort, request
 from models import storage
 from models.state import State
 from api.v1.views import app_views
+
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
